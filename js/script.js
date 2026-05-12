162fa80d0ec082b5385ea560b27d91db22dff036
flacha33 <jhonatanismael2010@gmail.com>
2026-05-12 09:59:00 -0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function verificarLogin() {
+  let login = document.getElementById("login").value;
+  let senha = document.getElementById("senha").value;
+  if (login === "admin" && senha == 123456){
+    alert("Acesso Liberado")
+    window.location.href="lanches.html";
+  } else {
+    alert("Acesso Negado")
+  }
+  
+  login = document.getElementById("login").value ='';
+  senha = document.getElementById("senha").value ='';
+}
